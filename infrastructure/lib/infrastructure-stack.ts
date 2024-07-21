a2c4f0e5b0547acd2b11cb6b40d63bc00b1a0b7a
sneguroma <sneguroma@gmail.com>
2024-07-21 20:29:08 +0200

    feat: changed stack and lambda

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -3,15 +3,29 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
+import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    /* const nestLambda = new lambda.Function(this, 'NestLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda.zip')), 
+      handler: 'dist/src/main.lambda.handler', 
+      environment: {
+        DATABASE_HOST: 'bakerydb.czuueys6ip67.eu-west-1.rds.amazonaws.com',
+        DATABASE_PORT: '5432', 
+        DATABASE_USER: 'postgres',
+        DATABASE_PASSWORD: 'postgres',
+        DATABASE_NAME: 'bakerydb',
+      },
+    }); */
+
     const nestLambda = new lambda.Function(this, 'NestLambda', {
       runtime: lambda.Runtime.NODEJS_20_X,
-      code: lambda.Code.fromAsset(path.join(__dirname, '../../dist')), 
-      handler: 'main.lambda.handler', 
+      handler: 'src/main.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../dist')),
       environment: {
         DATABASE_HOST: 'bakerydb.czuueys6ip67.eu-west-1.rds.amazonaws.com',
         DATABASE_PORT: '5432', 
@@ -20,6 +34,11 @@ export class InfrastructureStack extends cdk.Stack {
         DATABASE_NAME: 'bakerydb',
       },
     });
+
+    
+    /* new apigateway.LambdaRestApi(this, 'NestApi', {
+      handler: lambdaFunction,
+    }); */
 
     
     const api = new apigateway.LambdaRestApi(this, 'NestApi', {
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
+import { Callback,Handler, Context } from 'aws-lambda';
+import serverlessExpress from '@codegenie/serverless-express';
+
+let server: Handler;
+
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
@@ -14,8 +19,23 @@ async function bootstrap() {
   });
   app.use(helmet());
 
-  await app.listen(port);
+  /* await app.listen(port); */
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+
 }
 bootstrap().then(() => {
   console.log('App is running on %s port', port);
 });
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback,) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+
+
+
+
+
